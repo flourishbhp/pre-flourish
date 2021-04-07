7c6b22482847144ab0271f24e0a67fffa60378c6
tumiemosweu <imosweuk@gmail.com>
2021-04-07 11:06:36 +0200

    update reference config

--- pre_flourish/reference_model_configs.py
+++ pre_flourish/reference_model_configs.py
@@ -2,4 +2,5 @@ from edc_reference import site_reference_configs
 
 site_reference_configs.register_from_visit_schedule(
     visit_models={
-        'edc_appointment.appointment': ['pre_flourish.preflourishcaregivervisit']})
+        'edc_appointment.appointment': ['pre_flourish.preflourishcaregivervisit'],
+        'flourish_child.appointment': ['flourish_child.childvisit'], })
