19b8f3ce8a1ae0b4261f574cccac25f723361308
tumiemosweu <imosweuk@gmail.com>
2021-05-09 17:33:22 +0200

    update visit schedule

--- pre_flourish/visit_schedules/schedules/child_schedule.py
+++ pre_flourish/visit_schedules/schedules/child_schedule.py
@@ -24,25 +24,3 @@ visit0 = Visit(
     facility_name='5-day clinic')
 
 pre_flourish_child_schedule1.add_visit(visit=visit0)
-
-# schedule for new participants
-pre_flourish_child_schedule2 = Schedule(
-    name='pre_flourish_child_schedule2',
-    verbose_name='Pre Flourish Schedule 2',
-    onschedule_model='pre_flourish.onschedulechildpreflourish',
-    offschedule_model='pre_flourish.preflourishoffstudy',
-    consent_model='pre_flourish.caregiverchildscreeningconsent',
-    appointment_model='edc_appointment.appointment')
-
-pre_flourish_child_schedule2.add_visit(visit=visit0)
-
-# schedule for new participants
-pre_flourish_child_schedule3 = Schedule(
-    name='pre_flourish_child_schedule3',
-    verbose_name='Pre Flourish Schedule 3',
-    onschedule_model='pre_flourish.onschedulechildpreflourish',
-    offschedule_model='pre_flourish.preflourishoffstudy',
-    consent_model='pre_flourish.caregiverchildscreeningconsent',
-    appointment_model='edc_appointment.appointment')
-
-pre_flourish_child_schedule3.add_visit(visit=visit0)
