b41f60528ca736b6e13bdb4a9891bf26c27936ab
Moses Chawawa <moseschawawa@gmail.com>
2022-04-07 11:57:50 +0200

    Listboard added

--- pre_flourish/views/__init__.py
+++ pre_flourish/views/__init__.py
@@ -2,6 +2,7 @@ from .administration_view import AdministrationView
 from .caregiver import DashboardView
 from .caregiver import ScreeningListBoardView
 from .caregiver import SubjectListboardView
+from .caregiver import PreFlourishMaternalDatasetListBoardView
 from .child import ChildListboardView
 from .home_view import HomeView
 from .mixins import AppConfigListboardUrlsViewMixin
