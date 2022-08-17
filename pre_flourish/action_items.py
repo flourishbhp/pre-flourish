b597fac682671e4ee36cda1afbdee7adf9f632fc
Moses Chawawa <moseschawawa@gmail.com>
2022-08-17 16:51:21 +0200

    fixed action items

--- pre_flourish/action_items.py
+++ pre_flourish/action_items.py
@@ -1,10 +1,6 @@
 from edc_action_item import Action, site_action_items, HIGH_PRIORITY
 
-from edc_locator.action_items import SubjectLocatorAction
-
 MATERNALOFF_STUDY_ACTION = 'submit-caregiver-study'
-PRE_FLOURISH_CAREGIVER_LOCATOR_ACTION = 'pre-flourish-submit-caregiver-locator'
-
 
 class MaternalOffStudyAction(Action):
     name = MATERNALOFF_STUDY_ACTION
@@ -13,13 +9,3 @@ class MaternalOffStudyAction(Action):
     admin_site_name = 'pre_flourish_admin'
     priority = HIGH_PRIORITY
     singleton = True
-
-
-class PreFlourishCaregiverLocatorAction(SubjectLocatorAction):
-    name = PRE_FLOURISH_CAREGIVER_LOCATOR_ACTION
-    display_name = 'Submit Pre Flourish Caregiver Locator'
-    reference_model = 'pre_flourish.preflourishcaregiverlocator'
-    admin_site_name = 'pre_flourish_admin'
-
-
-site_action_items.register(PreFlourishCaregiverLocatorAction)
