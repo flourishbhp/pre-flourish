52f519ec0ebde4b2ebe04fea114dce9e91618545
Moses Chawawa <moseschawawa@gmail.com>
2022-08-24 22:37:22 +0200

    ADD : subject identifeir to be compulsory

--- pre_flourish/forms/child/pre_flourish_child_assent_form.py
+++ pre_flourish/forms/child/pre_flourish_child_assent_form.py
@@ -7,6 +7,11 @@ from ...models import PreFlourishChildAssent
 class PreFlourishChildAssentForm(
         SiteModelFormMixin, FormValidatorMixin, forms.ModelForm):
 
+    subject_identifier = forms.CharField(
+        label='Subject Identifier',
+        widget=forms.TextInput(attrs={'readonly': 'readonly'}),
+        required=False)
+
     class Meta:
         model = PreFlourishChildAssent
         fields = '__all__'
