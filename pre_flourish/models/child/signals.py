e31d3945eb7577dd3de1d2159f4f190fa27d1a79
salahdin <salahdinjemal@gmail.com>
2023-04-17 08:00:50 +0200

    refactor signals

--- pre_flourish/models/child/signals.py
+++ pre_flourish/models/child/signals.py
@@ -4,22 +4,15 @@ from django.dispatch import receiver
 
 from edc_visit_schedule.site_visit_schedules import site_visit_schedules
 
-from pre_flourish.models.child.pre_flourish_child_dummy_consent import PreFlourishChildDummySubjectConsent
-from pre_flourish.models.child.pre_flourish_child_assent import PreFlourishChildAssent
-from pre_flourish.models.child.pre_flourish_child_consent import \
-    PreFlourishCaregiverChildConsent
+from pre_flourish.models.child import PreFlourishChildDummySubjectConsent
+from pre_flourish.models.child import PreFlourishChildAssent
+from pre_flourish.models.child import PreFlourishCaregiverChildConsent
 
 
 class CaregiverConsentError(Exception):
     pass
 
 
-@receiver(post_save, weak=False, sender=PreFlourishCaregiverChildConsent,
-          dispatch_uid='pre_flourish_caregiver_child_consent_on_post_save')
-def pre_flourish_caregiver_child_consent_on_post_save(sender, instance, raw, created,
-                                                      **kwargs):
-    if instance.is_eligible:
-        create_child_dummy_consent(instance)
 
 
 @receiver(post_save, weak=False, sender=PreFlourishChildAssent,
@@ -46,13 +39,12 @@ def child_assent_on_post_save(sender, instance, raw, created, **kwargs):
 def pre_flourish_child_dummy_consent_on_post_save(sender, instance, raw, created, **kwargs):
     """Put subject on schedule after consenting.
     """
-    if True:
+    if not raw and instance.is_eligible:
         put_on_schedule(instance=instance,
                         subject_identifier=instance.subject_identifier,
                         onschedule_model='pre_flourish.onschedulechildpreflourish',
-                        schedule_name='pre_flourish_child_schedule1',
-                        base_appt_datetime=instance.consent_datetime.replace(
-                            microsecond=0)
+                        schedule_name='pf_child_schedule1',
+                        base_appt_datetime=instance.consent_datetime
                         )
 
 
@@ -63,21 +55,20 @@ def create_child_dummy_consent(instance, caregiver_child_consent_obj=None):
             subject_identifier=instance.subject_identifier)
     except PreFlourishChildDummySubjectConsent.DoesNotExist:
         PreFlourishChildDummySubjectConsent.objects.create(
-            subject_identifier=instance.subject_identifier,
-            consent_datetime=instance.consent_datetime,
-            identity=instance.identity,
-            dob=instance.dob,
+            subject_identifier=caregiver_child_consent_obj.subject_identifier,
+            consent_datetime=caregiver_child_consent_obj.consent_datetime,
+            identity=caregiver_child_consent_obj.identity,
+            dob=caregiver_child_consent_obj.dob,
         )
 
 
 def put_on_schedule(instance=None, subject_identifier=None,
                     base_appt_datetime=None, onschedule_model=None, schedule_name=None):
-    if instance:
-        _, schedule = site_visit_schedules.get_by_onschedule_model_schedule_name(
-            onschedule_model=onschedule_model, name=schedule_name)
+    _, schedule = site_visit_schedules.get_by_onschedule_model_schedule_name(
+        onschedule_model=onschedule_model, name=schedule_name)
 
-        schedule.put_on_schedule(
-            subject_identifier=subject_identifier,
-            onschedule_datetime=base_appt_datetime,
-            schedule_name=schedule_name,
-            base_appt_datetime=base_appt_datetime)
+    schedule.put_on_schedule(
+        subject_identifier=subject_identifier,
+        onschedule_datetime=base_appt_datetime,
+        schedule_name=schedule_name,
+        base_appt_datetime=base_appt_datetime)
