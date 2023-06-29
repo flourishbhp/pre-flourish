0d6cf0711f79cb61709a1a097d49847977c2eb84
Keba462 <79043038+Keba462@users.noreply.github.com>
2023-06-29 09:46:17 +0200

    Created a validation for dob against date today and added tests when the dob is in valid and when its valid(child_age equals or above 7 years of age)

---
+++ pre_flourish/tests/test_pre_flourish_child_consent_validation.py
@@ -0,0 +1,49 @@
+from django.test import TestCase, tag
+from edc_facility.import_holidays import import_holidays
+from model_mommy import mommy
+from edc_base import get_utcnow
+from dateutil.relativedelta import relativedelta
+from datetime import datetime
+from django.core.exceptions import ValidationError
+from pre_flourish.form_validators.caregiver_child_consent_form_validator import PreFlourishCaregiverChildConsentFormValidator
+
+@tag('cc')
+class TestChildConsentForm(TestCase):
+
+    
+    def setUp(self):
+        import_holidays()
+
+        self.caregiver_screening = mommy.make_recipe(
+            'pre_flourish.preflourishsubjectscreening', )
+
+        self.subject_consent = mommy.make_recipe(
+            'pre_flourish.preflourishconsent',
+            screening_identifier=self.caregiver_screening.screening_identifier,
+        )
+        
+
+    def test_dob_invalid(self):
+        caregiver_child_consent = {
+
+            'child_dob':datetime.now().date()
+        }
+
+        form_validator = PreFlourishCaregiverChildConsentFormValidator(
+                cleaned_data=caregiver_child_consent)
+        self.assertRaises(ValidationError, form_validator.validate)
+        self.assertIn('child_dob', form_validator._errors)
+
+    def test_dob_valid(self):
+        caregiver_child_consent = {
+
+            'child_dob':(get_utcnow() - relativedelta(years=7)).date(),
+        }
+
+        form_validator = PreFlourishCaregiverChildConsentFormValidator(
+                cleaned_data=caregiver_child_consent)
+        try:
+            form_validator.validate()
+        except ValidationError as e:
+            self.fail(f'ValidationError unexpectedly raised. Got{e}')
+
