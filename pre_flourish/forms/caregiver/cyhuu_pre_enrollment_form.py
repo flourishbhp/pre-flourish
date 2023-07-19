2215c2227707cfccb56e30265d78a3e0203761ce
Keba462 <79043038+Keba462@users.noreply.github.com>
2023-07-19 11:36:03 +0200

    Prefilled Q3 Using screening_obj

--- pre_flourish/forms/caregiver/cyhuu_pre_enrollment_form.py
+++ pre_flourish/forms/caregiver/cyhuu_pre_enrollment_form.py
@@ -1,5 +1,5 @@
 from django import forms
-
+from django.apps import apps as django_apps
 from .form_mixins import SubjectModelFormMixin
 from ...form_validators import CyhuuPreEnrollmentFormValidator
 
@@ -9,6 +9,44 @@ from ...models import CyhuuPreEnrollment
 class CyhuuPreEnrollmentForm(SubjectModelFormMixin, forms.ModelForm):
 
     form_validator_cls = CyhuuPreEnrollmentFormValidator
+    screening_model = 'pre_flourish.preflourishsubjectscreening'
+    consent_model = 'pre_flourish.preflourishconsent'
+
+    @property
+    def screening_model_cls(self):
+        return django_apps.get_model(self.screening_model)
+
+    @property
+    def consent_model_cls(self):
+        return django_apps.get_model(self.consent_model)
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        self.subject_identifier = self.initial.get(
+            'subject_identifier', None)
+
+        self.initial['biological_mother'] = getattr(
+            self.screening_obj, 'biological_mother')
+
+    @property
+    def screening_obj(self):
+        try:
+            return self.screening_model_cls.objects.get(
+                screening_identifier=self.consent_obj_screening
+            )
+        except self.screening_model_cls.DoesNotExist:
+            raise
+
+    @property
+    def consent_obj_screening(self):
+        try:
+            return self.consent_model_cls.objects.get(
+                subject_identifier=self.subject_identifier
+            ).screening_identifier
+
+        except self.consent_model_cls.DoesNotExist:
+            raise
 
     class Meta:
         model = CyhuuPreEnrollment
